06db917d6c67c10205805a87a0e974311ea5736b
Rafael Donizete Basso <rafael.basso@onlineapp.com.br>
2022-03-02 20:41:57 -0300

    upload project

---
+++ CursoMVC/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=true");
+        }
+    }
+}
